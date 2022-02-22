24ecf7b689ec0c7ae64f0a496e7090c81a415bd8
noordseoostende <voropayev1@mail.ru>
2022-02-22 16:57:03 +0100

    useGlobalContext)(...)' as it is undefined.     at App (bundle.js:38:5)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,35 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
-  return <h2>toets nieuwe</h2>
+  const {waiting, loading,questions,index,correct} = useGlobalContext()
+  if (waiting) {
+    
+    return <SetupForm />
+  }
+  if (loading) {
+    return <Loading />
+  }
+  console.log(questions)
+  const {question, incorrect_answers,correct_answer} = questions[index];
+  const answers = [...incorrect_answers, correct_answer];
+  return <main>
+    <Modal />
+    <section className="quiz">
+      <p className="correct-answers">
+        correct answers : {correct}/{index}
+      </p>
+      <article className="container">
+
+      <h2 dangerouslySetInnerHTML={{__html:question}} />
+      <div className="btn-container">
+        {answers.map((answer,index)=>{
+          return <button key={index} className="answer-btn" dangerouslySetInnerHTML={{__html:answer}} />
+        })}
+      </div>
+      </article>
+      <button className="next-question">next question</button>
+    </section>
+  </main>
 }
 
 export default App
